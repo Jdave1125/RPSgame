5e9de6ecc56e5e8aa66504fbe5cc538e5cfbd940
Jdave1125 <110739162+Jdave1125@users.noreply.github.com>
2023-01-09 20:05:46 -0500

    Add files via upload

---
+++ rockpap.js
@@ -0,0 +1,83 @@
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }else{
+        return "Computer";
+    }
+    
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "Tie"
+    }
+    else if(result == "Player"){
+        return `You the man ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You lost, better luck next time ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+
+function getPlayerChoice(){
+    let validatedInput = false;
+    while (validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if (choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
+
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!")
+    for (let i=0;i<5;i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("next round mortal -");
+        if(checkWinner(playerSelection,computerSelection) == "Player"){
+            scorePlayer++;
+        }
+        else if(checkWinner(playerSelection,computerSelection) == "Computer"){
+            scoreComputer++;
+        }
+    }
+    console.log("Game Over")
+    if(scorePlayer>scoreComputer){
+        console.log("PLAYER IS VICTORIOUS");
+    }
+    else if(scoreComputer>scorePlayer){
+        console.log("THE MACHINES WILL RISE AGAINST HUMANITY");
+    }
+    else{
+        console.log("AGAIN - you've tied");
+    }
+
+}
+
+game()
